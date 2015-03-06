1ea55c2ab943ef3fb311881721526f96920e9433
Stephanie Yu <styu314@gmail.com>
2015-03-05 22:17:11 -0500

    Function to truncate post

--- wordpress/wp-content/themes/mitua/front-page.php
+++ wordpress/wp-content/themes/mitua/front-page.php
@@ -8,20 +8,31 @@ $url = "";
 get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
-      <?php while ( have_posts() ) : the_post(); 
-        if ( has_post_thumbnail() ) {
-          $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-        } else {
-          // Default URL Goes here
-          $url = '';
-        }
+  <?php while ( have_posts() ) : the_post(); 
+    if ( has_post_thumbnail() ) {
+      $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+    } else {
+      // Default URL Goes here
+      $url = '';
+    }
 
-        break;
-      ?>
-      <?php endwhile; ?>
-    <?php else : ?>
-      <?php //get_template_part( 'content', 'none' ); ?>
-    <?php endif; ?>
+    break;
+  ?>
+  <?php endwhile; ?>
+<?php else : ?>
+  <?php //get_template_part( 'content', 'none' ); ?>
+<?php endif; ?>
+
+<?php
+  function firstSentence($content) {
+    if(preg_match('/[.?!]/', $content, $matches, PREG_OFFSET_CAPTURE)) {
+        $index = $matches[0][1];
+        return substr($content, 0, $index + 1);
+    } else {
+        return $content;
+    }
+  }
+?>
 
 <div class="feature">
   <div class="feature-background" style="background-image:url('<?php echo $url; ?>');">
@@ -30,7 +41,7 @@ get_header(); ?>
     <div class="large-12 columns feature-text-container">
       <div class="feature-text">
         <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <p><?php echo firstSentence(get_the_content()); ?></p>
         <a class="more-link" href="<?php the_permalink() ?>">read more</a>
       </div>
     </div>
