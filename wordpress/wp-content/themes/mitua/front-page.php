8ba4c1524afc3477c58fc823460af8b452cddb63
poofytoo <victorhung92@gmail.com>
2015-03-05 21:48:34 -0500

    front page added full photo

--- wordpress/wp-content/themes/mitua/front-page.php
+++ wordpress/wp-content/themes/mitua/front-page.php
@@ -23,40 +23,43 @@ get_header(); ?>
       <?php //get_template_part( 'content', 'none' ); ?>
     <?php endif; ?>
 
-<div class="row feature">
+<div class="feature">
   <div class="feature-background" style="background-image:url('<?php echo $url; ?>');">
   </div>
-  <div class="large-12 columns feature-text-container">
-    <div class="feature-text">
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <a class="more-link" href="<?php the_permalink() ?>">read more</a>
+  <div class="row">
+    <div class="large-12 columns feature-text-container">
+      <div class="feature-text">
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+        <a class="more-link" href="<?php the_permalink() ?>">read more</a>
+      </div>
     </div>
   </div>
 </div>
 
 <div class="who-we-are">
   <div class="row">
-  <div class="medium-1 columns text-graphic-container">
-    <div class="text-graphic">
+    <div class="medium-1 columns text-graphic-container">
+      <div class="text-graphic">
+      </div>
+      &nbsp;
     </div>
-    &nbsp;
-  </div>
-  <div class="medium-6 columns text-desc">
-    <h3>Who We Are</h3>
-    <p>The UA is a group of undergraduate students elected to represent the MIT student body and all its facets.</p>
-  </div>
-  <div class="medium-4 columns text-link-container">
-    <a class="more-link" href="">Meet the 2015 UA</a>
+    <div class="medium-6 columns text-desc">
+      <h3>Who We Are</h3>
+      <p>The UA is a group of undergraduate students elected to represent the MIT student body and all its facets.</p>
+    </div>
+    <div class="medium-4 columns text-link-container">
+      <a class="more-link" href="">Meet the 2015 UA</a>
+    </div>
   </div>
 </div>
 
 <div class="twitter">
   <div class="row">
-    <div class="large-4 columns">
-
+    <div class="medium-4 columns">
+      &nbsp;
     </div>
-    <div class="large-8 columns text-desc">
+    <div class="medium-8 columns text-desc">
       Our Public Relations Chair, Connie Huang, dishing out ice cream at the UGC-UA Toscanini's Study Break! #UA #UGC #MIT
     </div>
   </div>
